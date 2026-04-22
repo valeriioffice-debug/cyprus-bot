5ef6723f30d61ac58f6a3ae507b3f68243f801d2
valeriioffice-debug <valerii.office@gmail.com>
2026-04-22 14:01:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,17 @@
-print('bot')
+import telebot
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Бот запущен 🚀")
+
+@bot.message_handler(commands=['status'])
+def status(message):
+    bot.send_message(message.chat.id, "Статус: работает ✅")
+
+print("Bot is running...")
+bot.infinity_polling()
