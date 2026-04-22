1c2ce05533ef7e0aad12a2afa3ae259bff1db800
valeriioffice-debug <valerii.office@gmail.com>
2026-04-22 17:45:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,9 +26,9 @@ print("Bot is running...")
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
     if message.text == "📅 Сегодня":
-    today = datetime.now().strftime("%d.%m.%Y")
+        today = datetime.now().strftime("%d.%m.%Y")
 
-    bot.send_message(
+        bot.send_message(
     message.chat.id,
     f"📅 Сегодня ({today})\n\n"
     "✈️ EUROCONTROL:\n"
